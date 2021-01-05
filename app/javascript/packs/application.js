5c78927e8ff7db6e6f670c3db09623a5950561e4
vijay <vijaymallela88@gmail.com>
2021-01-06 00:47:25 +0530

    first change

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,13 @@
 // compiled file.
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
 //
 //= require turbolinks
 //= require shortcut.js
-//= require_tree .
+
+
